22ca8aefa83f3012ed5e05f99284682c4ef0774e
Daniel Corral <dani05corral@gmail.com>
2025-02-19 18:18:30 +0100

    cambios menores

--- src/main/kotlin/com/es/aplicacion/model/Usuario.kt
+++ src/main/kotlin/com/es/aplicacion/model/Usuario.kt
@@ -3,7 +3,7 @@ package com.es.aplicacion.model
 import org.bson.codecs.pojo.annotations.BsonId
 import org.springframework.data.mongodb.core.mapping.Document
 
-@Document("usuario")
+@Document("usuarios")
 data class Usuario(
     @BsonId
     val _id: String?,
